d164e60941ebd5bf3b00d8493b8953ec13b8f826
Laure <laure.milian@gmail.com>
2017-01-06 12:08:44 +0100

    step5 (complete) - enregistrement du dernier message dans last_message.json

--- pages/contact.php
+++ pages/contact.php
@@ -2,4 +2,6 @@
 
 <form action="/save.php" method="post">
 	<input name="username">
+	<input name="message">
+	<input type="submit" value="submit">
 </form>
--- public/save.php
+++ public/save.php
@@ -1,3 +1,8 @@
 <?php
-echo $_POST["username"];
+	//echo $last_message;
+	$last_message = json_encode();
+	$nom = $_POST["username"];
+	$message = $_POST["message"];
+	var_dump($data);
+	file_put_contents(__DIR__.'/../data/last_message.json', '{"nom": "'.$nom.'", "message": "'.$message.'"}', FILE_USE_INCLUDE_PATH);
 ?>
